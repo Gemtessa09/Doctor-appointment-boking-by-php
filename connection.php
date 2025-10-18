85d575690f5a6c35244b02ee485e24014b67fb24
Gemtessa Deksisa <gemtessadeksisa@gmail.com>
2025-10-19 02:45:32 +0900

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,17 +1,17 @@
 <?php
-$host = "dpg-d3ps9rqli9vc73bs6lvg-a";
+// Connect to Render PostgreSQL database
+$host = "dpg-d3psv37diees73c4aq3g-a.oregon-postgres.render.com";
 $port = "5432";
-$dbname = "edoc_xt4w";
-$user = "edoc_xt4w_user";
-$password = "WmMydYKl5PNVo486cPRjROTjZVByJ1YP";
+$dbname = "edoc_cg1i";
+$username = "edoc_cg1i_user";
+$password = "GoE9Thow9mcrmAqoibgttxJzvDaqoHB4";
 
 // Create connection
-$database = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+$database = new mysqli($host, $username, $password, $dbname, $port);
 
-if (!$database) {
-    die("Connection failed: " . pg_last_error());
+// Check connection
+if ($database->connect_error) {
+    die("Connection failed: " . $database->connect_error);
 }
 
-// Optional: set client encoding to UTF8
-pg_set_client_encoding($database, "UTF8");
 ?>
