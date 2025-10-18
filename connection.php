e973dda2c877acd40ddd62632db58b1ac2c69b75
Gemtessa Deksisa <gemtessadeksisa@gmail.com>
2025-10-19 01:52:02 +0900

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,17 @@
 <?php
+$host = "dpg-d3ps9rqli9vc73bs6lvg-a";
+$port = "5432";
+$dbname = "edoc_xt4w";
+$user = "edoc_xt4w_user";
+$password = "WmMydYKl5PNVo486cPRjROTjZVByJ1YP";
 
-    $database= new mysqli("localhost","root","","edoc");
-    if ($database->connect_error){
-        die("Connection failed:  ".$database->connect_error);
-    }
+// Create connection
+$database = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
+if (!$database) {
+    die("Connection failed: " . pg_last_error());
+}
+
+// Optional: set client encoding to UTF8
+pg_set_client_encoding($database, "UTF8");
 ?>
